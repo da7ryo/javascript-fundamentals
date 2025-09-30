4422944a6eb78069f6ae01f67b16426664ce7df0
Dario Milanović <da7ryo@gmail.com>
2025-09-30 17:33:55 +0200

    Introduce variables and operators

--- 1-valuesAndDataTypesAndVariables.js
+++ 1-valuesAndDataTypesAndVariables.js
@@ -1,3 +1,44 @@
-console.log("Hello world"); // string data type 
+// **** VALUES AND DATA TYPES ****
+
+console.log('Hello world'); // string data type
 console.log(5); // number data type
 console.log(true); // boolean data type
+
+// **** VARIABLES ****
+
+const favouriteNumber = 15;
+console.log(favouriteNumber);
+console.log(favouriteNumber);
+console.log(favouriteNumber);
+
+let fullName = 'Marija Biletic';
+console.log(fullName);
+fullName = 'Marija Milanovic';
+console.log(fullName);
+
+// There is also var variables, but this is only used in older codebases
+
+let age; // Value is undefined and data type of undefined is undefined
+console.log(age);
+age = 15;
+
+let person = null;
+console.log(person);
+person = 'Mateo Vukovic';
+console.log(person);
+
+// **** OPERATORS ****
+
+// Operator is a symbol that does some operation on a value or multiple values and produces a final value
+console.log(typeof 'Hello');
+console.log(typeof 1000);
+console.log(typeof false);
+
+// Math operators
+console.log(3 + 5);
+const num1 = 10;
+const num2 = 5;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
