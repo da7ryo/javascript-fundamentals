73e06791db170f2877fa42e0877531207b0c9c50
Dario Milanović <da7ryo@gmail.com>
2025-10-01 18:02:18 +0200

    Introduce other operators and if/else statement

--- 1-valuesAndDataTypesAndVariables.js
+++ 1-valuesAndDataTypesAndVariables.js
@@ -42,3 +42,44 @@ console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
+
+// **** ASSIGMENT OPERATORS ****
+
+let num3 = 10;
+num3 = num3 + 1;
+num3++;
+num3 = num3 - 1;
+num3--;
+num3 = num3 + 5;
+num3 += 5;
+num3 = num3 * 7;
+num3 *= 7;
+
+// **** COMPARISON OPERATORS ****
+
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 >= 3);
+console.log(5 <= 3);
+console.log(5 === 6);
+console.log(5 !== 6);
+
+console.log((3 + 4) * 5);
+
+// **** IF/ELSE STATEMENTS ****
+
+console.log('**** IF/ELSE STATEMENTS ****');
+
+const isFootballPlayer = false;
+
+if (isFootballPlayer) {
+  console.log('He will be the next Ronaldo');
+}
+
+console.log('Pikachu');
+const isActionMovie = false;
+if (isActionMovie) {
+  console.log('This is an action movie');
+} else {
+  console.log('This is not an action movie');
+}
