979b990c9a3cbd3f9596e942a54d3d2b3d620ee1
Dario Milanović <da7ryo@gmail.com>
2025-12-01 18:04:52 +0100

    Introduce in async code

--- 17-async.js
+++ 17-async.js
@@ -1,9 +1,40 @@
+const fs = require('fs');
+
 setTimeout(() => {
   console.log('Hello');
-}, 5000);
+}, 0);
 
 /* setTimeout(function () {
   console.log('Hello');
  }, 5000); */
 
 console.log('Hi');
+
+fs.readFile('testData1.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log('Something went wrong, couldn´t read file');
+    return;
+  }
+
+  console.log(data);
+  fs.readFile('testData2.txt', 'utf-8', (err, data) => {
+    if (err) {
+      console.log('Something went wrong, couldn´t read file');
+      return;
+    }
+
+    console.log(data);
+    fs.readFile('testData3.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log('Something went wrong, couldn´t read file');
+        return;
+      }
+
+      console.log(data);
+    });
+  });
+});
+
+console.log('pikachu');
+
+// **** Promise objects ****
