55400ad2a20fe5a39f115d077fe675f17d58c9fa
Dario Milanović <da7ryo@gmail.com>
2026-01-13 18:57:08 +0100

    Fourth day of training for test

--- 24-test2.js
+++ 24-test2.js
@@ -29,8 +29,20 @@ class InventoryManager {
       action(product);
     });
   }
-}
 
+  getLowStockProducts() {
+    /* const newArr = [];
+    for (let i = 0; i < this.items.length; i++) {
+      const currentProduct = this.items[i];
+      if (currentProduct.isLowStock(8)) {
+        newArr.push(currentProduct);
+      }
+    }
+    return newArr;
+   */
+    return this.items.filter((item) => item.isLowStock(8));
+  }
+}
 const inventoryManager = new InventoryManager();
 
 const p1 = new Product(1, 'laptop', 1200, 10);
@@ -39,3 +51,27 @@ const p2 = new Product(2, 'Tv', 650, 5);
 inventoryManager.addProduct(p1);
 inventoryManager.addProduct(p2);
 inventoryManager.addProduct(new Product(3, 'Phone', 700, 10));
+
+// console.log(inventoryManager.items);
+
+const alertSystem = function (product) {
+  if (product.isLowStock(8)) {
+    console.log(`ALERT: ${product.name} is low! Only ${product.stock} left.`);
+  }
+};
+
+inventoryManager.processInventory(alertSystem);
+
+console.log(inventoryManager.getLowStockProducts());
+const lowStockProducts = inventoryManager.getLowStockProducts();
+
+if (lowStockProducts.length) {
+  const firstLowStockProduct = lowStockProducts[0];
+  firstLowStockProduct.name = 'Leteca metla';
+}
+console.log(inventoryManager.items);
+
+const arr = [1, 2, 3];
+arr2 = new Array(1, 2, 3);
+
+arr.push(); // ipisati Hello world
